76b19871c6bc9a6bf2c9f04384ebe51affde0a10
ANSTUDIO <archangellus@users.noreply.github.com>
2026-04-28 20:02:40 +0300

    Update MultipleQuestTrigger to run sequential quest list

--- Assets/RPG/Scripts/MultipleQuestTrigger.cs
+++ Assets/RPG/Scripts/MultipleQuestTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PLAYERTWO.ARPGProject
@@ -6,12 +7,26 @@ namespace PLAYERTWO.ARPGProject
     [AddComponentMenu("PLAYER TWO/ARPG Project/Quest/Multiple Quest Trigger")]
     public class MultipleQuestTrigger : MonoBehaviour
     {
-        [Tooltip("The Quest you want to trigger completion.")]
-        public Quest quest;
+        [Tooltip("The Quest sequence to start. Each quest starts after the previous one is completed.")]
+        public List<Quest> quests = new();
 
         protected Collider m_collider;
+        protected int m_currentQuestIndex = -1;
+        protected bool m_sequenceStarted;
 
         protected QuestsManager m_manager => Game.instance.quests;
+
+        protected virtual void OnEnable()
+        {
+            if (Game.instance)
+                m_manager.onQuestCompleted += OnQuestCompleted;
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (Game.instance)
+                m_manager.onQuestCompleted -= OnQuestCompleted;
+        }
 
         protected virtual void InitializeCollider()
         {
@@ -23,10 +38,51 @@ namespace PLAYERTWO.ARPGProject
 
         protected virtual void OnTriggerEnter(Collider other)
         {
-            if (!other.IsPlayer())
+            if (!other.IsPlayer() || m_sequenceStarted)
                 return;
 
-            m_manager.Trigger(quest);
+            m_sequenceStarted = true;
+            StartNextQuest();
+        }
+
+        protected virtual void OnQuestCompleted(QuestInstance instance)
+        {
+            if (!m_sequenceStarted || m_currentQuestIndex < 0 || m_currentQuestIndex >= quests.Count)
+                return;
+
+            if (instance.data != quests[m_currentQuestIndex])
+                return;
+
+            StartNextQuest();
+        }
+
+        protected virtual void StartNextQuest()
+        {
+            var nextQuest = FindNextQuest();
+
+            if (!nextQuest)
+                return;
+
+            m_manager.AddQuest(nextQuest);
+
+            if (m_manager.TryGetQuest(nextQuest, out var instance) && instance.completed)
+                StartNextQuest();
+        }
+
+        protected virtual Quest FindNextQuest()
+        {
+            for (var i = m_currentQuestIndex + 1; i < quests.Count; i++)
+            {
+                var quest = quests[i];
+
+                if (!quest)
+                    continue;
+
+                m_currentQuestIndex = i;
+                return quest;
+            }
+
+            return null;
         }
     }
 }
