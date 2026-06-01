5c161fe68e9c2a59cf1f769818017746ebd264f7
ANSTUDIO <archangellus@users.noreply.github.com>
2026-06-01 21:15:42 +0300

    Revert "Add ItemMaterial type and enable AI gather filtering for materials"

--- Assets/PLAYER TWO/ARPG Project/Core/Items/ItemMaterial.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace PLAYERTWO.ARPGProject
-{
-    [CreateAssetMenu(fileName = "New Material", menuName = "PLAYER TWO/ARPG Project/Item/Material")]
-    public class ItemMaterial : Item
-    {
-    }
-}
