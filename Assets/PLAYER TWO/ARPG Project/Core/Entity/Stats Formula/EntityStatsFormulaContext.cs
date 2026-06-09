de021a796bab08d686dfacca7bd7a500a804d2bd
ANSTUDIO <archangellus@users.noreply.github.com>
2026-06-09 23:38:09 +0300

    Add built-in stat formula examples

---
+++ Assets/PLAYER TWO/ARPG Project/Core/Entity/Stats Formula/EntityStatsFormulaContext.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+
+namespace PLAYERTWO.ARPGProject
+{
+    public readonly struct EntityStatsFormulaContext
+    {
+        readonly Func<EntityStatsFormulaInput, float> m_getValue;
+
+        public EntityStatsFormulaContext(Func<EntityStatsFormulaInput, float> getValue)
+        {
+            m_getValue = getValue;
+        }
+
+        public float Get(EntityStatsFormulaInput input) => m_getValue != null ? m_getValue(input) : 0f;
+
+        public static EntityStatsFormulaContext Preview =>
+            new EntityStatsFormulaContext(input =>
+            {
+                switch (input)
+                {
+                    case EntityStatsFormulaInput.Level:
+                        return 1f;
+                    case EntityStatsFormulaInput.Strength:
+                        return 20f;
+                    case EntityStatsFormulaInput.Dexterity:
+                    case EntityStatsFormulaInput.Vitality:
+                        return 15f;
+                    case EntityStatsFormulaInput.Energy:
+                        return 10f;
+                    case EntityStatsFormulaInput.WeaponDamageMin:
+                        return 2f;
+                    case EntityStatsFormulaInput.WeaponDamageMax:
+                        return 5f;
+                    case EntityStatsFormulaInput.HealthMultiplier:
+                    case EntityStatsFormulaInput.ManaMultiplier:
+                    case EntityStatsFormulaInput.DamageMultiplier:
+                    case EntityStatsFormulaInput.MagicDamageMultiplier:
+                    case EntityStatsFormulaInput.CriticalMultiplier:
+                    case EntityStatsFormulaInput.DefenseMultiplier:
+                        return 1f;
+                    case EntityStatsFormulaInput.BaseExperience:
+                        return 100f;
+                    case EntityStatsFormulaInput.ExperiencePerLevel:
+                        return 50f;
+                    case EntityStatsFormulaInput.MaxAttackSpeed:
+                    case EntityStatsFormulaInput.MaxBlockSpeed:
+                    case EntityStatsFormulaInput.MaxStunSpeed:
+                        return 100f;
+                    case EntityStatsFormulaInput.MaxBlockChance:
+                        return 0.75f;
+                    case EntityStatsFormulaInput.MaxStunChance:
+                        return 0.75f;
+                    case EntityStatsFormulaInput.AccuracyBase:
+                        return 50f;
+                    default:
+                        return 0f;
+                }
+            });
+    }
+}
